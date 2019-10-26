d374889c6d3e8c8af826e597e73d84ece73d8943
lemondrok <dun2000@mail.ru>
2019-10-26 03:07:02 +0300

    server fixes

---
+++ server/main.py
@@ -0,0 +1,51 @@
+from aiohttp import web, hdrs
+
+
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
+
+
+def leo_algo(sss):
+    pass
+
+
+def leo_answer(t_from, t_to):
+
+    with open("./tron.wav", "rb") as input_file:
+        data = input_file.read()
+
+    return data[:8820000]
+
+
+async def upload_handler(request):
+    content = await request.content.read()
+    leo_algo(content)
+    return web.Response(status=200)
+
+
+async def response_handler(request):
+    time_from = request.match_info.get("from")
+    time_to = request.match_info.get("to")
+    response_byte_arr = leo_answer(time_from, time_to)
+
+    return web.Response(body=response_byte_arr)
+
+
+async def main_page_handler(request):
+    return web.Response(status=200)
+
+
+async def on_prepare(_, response):
+    response.headers[hdrs.ACCESS_CONTROL_ALLOW_ORIGIN] = '*'
+
+
+app = web.Application()
+app.on_response_prepare.append(on_prepare)
+app.add_routes([web.post('/upload', upload_handler),
+                web.get(r'/from{from}to{to}', response_handler),
+                web.get('/', main_page_handler)])
+
+web.run_app(app, port=os.getenv('PORT', 5000))
