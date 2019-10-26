45def59306d4c20289c1d406cf6eaab5666ec7e4
AndBondStyle <andbondstyle@gmail.com>
2019-10-26 03:10:55 +0300

    Fetch function

--- client/src/scripts/main.ts
+++ client/src/scripts/main.ts
@@ -2,6 +2,16 @@ import { Audio } from './audio';
 
 document.onload = main;
 
+const url = 'http://box-server.herokuapp.com/from1to1';
+const sampleRate = 44100;
+
+async function fetchChunk() {
+    let res = await fetch(url);
+    let buff = await res.arrayBuffer();
+    return new Float32Array(buff);
+}
+
 function main() {
-    const audio = new Audio(new AudioContext({ sampleRate: 44100 }));
+    const ctx = new AudioContext({ sampleRate });
+    const audio = new Audio(ctx);
 }
