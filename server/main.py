8dedc59474df9380facdfe04e05fe58870a516f4
lemondrok <dun2000@mail.ru>
2019-10-26 22:46:26 +0300

    fix my algo

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from scipy.fftpack import rfft
 from functools import partial
 import logging
 import os
+import math
 import numpy as np
 import tempfile
 import asyncio
@@ -19,12 +20,11 @@ logger = logging.getLogger(__name__)
 
 
 def polynomial_hash(arr, p):
-    base = 1
-    ans = 0
+    base = 1.
+    ans = 0.
     for i in range(len(arr)):
-        ans += i * base
-        base *= p
-    return ans
+        ans += (arr[i] * arr[i])
+    return math.sqrt(ans)
 
 
 class PrecalcMediumFFTAlgo:
@@ -43,39 +43,31 @@ class PrecalcMediumFFTAlgo:
         windowed = util.view_as_blocks(freq_arr, (100, ))
         block_sum = np.array(list(map(np.sum, windowed)))
 
-        arr_hash = polynomial_hash(block_sum, 113)
-        arr_hash /= 1000
+        # arr_hash = polynomial_hash(block_sum, 47)
+        arr_hash = np.sum(block_sum)
+        # arr_hash = arr_hash
         arr_hash = round(arr_hash, 4)
 
-        return [arr_hash, from_sample]
-
-    # def _positional_value(self, window_size, first_start, second_start):
-    #     first = self.data[first_start:first_start + window_size]
-    #     second = self.data[second_start:second_start + window_size]
-    #     first_padded = np.zeros((window_size,) + self.data.shape[1:])
-    #     first_padded[:first.shape[0]] = first
-    #     second_padded = np.zeros((window_size,) + self.data.shape[1:])
-    #     second_padded[:second.shape[0]] = second
-    #     return self._mse(first_padded, second_padded)
+        return [arr_hash, from_sample + window_size / 2]
 
     def run(self):
         start_time = time.time()
-        window_size = 1000
-        stride = 64
-        hash_result = defaultdict(lambda: [])
+        window_size = 2 * 44000
+        stride = 500
+        hash_result = []
         result = []
         with ThreadPoolExecutor(16) as pool:
             def iteration(first_start):
                 hsh = self._get_fft_hash(first_start, window_size)
                 return hsh, first_start
 
-            def map_append(index_list):
-                res = []
-                for i in range(len(index_list)):
-                    for j in range(i + 1, len(index_list)):
-                        if abs(index_list[j] - index_list[i]) > np.float(self.sample_rate) * 2:
-                            res.append([index_list[i], index_list[j]])
-                return res
+            # def map_append(index_list):
+            #     res = []
+            #     for i in range(len(index_list)):
+            #         for j in range(i + 1, len(index_list)):
+            #             if abs(index_list[j] - index_list[i]) > np.float(self.sample_rate) * 2:
+            #                 res.append([index_list[i], index_list[j]])
+            #     return res
 
             for result_part in pool.map(
                 iteration, range(
@@ -83,14 +75,18 @@ class PrecalcMediumFFTAlgo:
                     len(self.one_channel) - window_size - len(self.one_channel) // 10, stride
                 )
             ):
-                hash_result[result_part[0][0]].append(result_part[0][1])
+                hash_result.append(result_part[0])
 
-            for result_part in pool.map(map_append, (item for _, item in hash_result.items())):
-                result.extend(result_part)
+        hash_result.sort()
+        max_hash = hash_result[-1][0]
+        for i in range(len(hash_result) - 1):
+            if hash_result[i + 1][0] - hash_result[i][0] < max_hash / 100000:
+                result.append([hash_result[i][1], hash_result[i + 1][1]])
 
         end_time = time.time()
         print(f'Finished PrecalcMediumFFTAlgo precalc, done in {int((end_time - start_time) * 1000)}ms.', flush=True)
-        logger.error(result)
+        logger.error(hash_result)
+        logger.error(len(result))
 
         return np.array(result, dtype=np.float) / np.float(self.sample_rate)
 
