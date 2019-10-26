7df0c3f38d6902f6f9188cf3e570e9d6653e7e6d
lemondrok <dun2000@mail.ru>
2019-10-26 10:33:09 +0300

    pig

--- server/main.py
+++ server/main.py
@@ -4,24 +4,26 @@ from tqdm import tqdm
 from functools import partial
 import logging
 import os
-import io
 import numpy as np
-import random
 import tempfile
 import asyncio
+import ffmpeg
+import time
+import random
 from concurrent.futures import ProcessPoolExecutor
 
 
 logger = logging.getLogger(__name__)
 
 
-class Algorithm:
+class PrecalcAlgo:
     def __init__(self, sample_rate, data):
         self.sample_rate = int(sample_rate)
-        self.data = np.array([data[:, i] / np.max(np.abs(data[:, i])) for i in range(data.shape[1])]).T
+        self.data = np.array(data)
+        self.data = np.array([self.data[:, i] / np.max(np.abs(self.data[:, i])) for i in range(self.data.shape[1])]).T
 
     def _mse(self, a, b):
-        return np.sum(np.power(a - b, 2))
+        return np.sqrt(np.sum(np.power(a - b, 2)))
 
     def _positional_value(self, window_size, first_start, second_start):
         first = self.data[first_start:first_start + window_size]
@@ -32,9 +34,10 @@ class Algorithm:
         second_padded[:second.shape[0]] = second
         return self._mse(first_padded, second_padded)
 
-    def edges(self, window_size=None, stride=None, threshold=1453.8958203047514):
-        window_size = window_size or 2 * self.sample_rate
-        stride = stride or self.sample_rate
+    def run(self):
+        window_size = 2 * self.sample_rate
+        stride = self.sample_rate
+        threshold = 100
         result = []
         for first_start in tqdm(range(0, self.data.shape[0], stride)):
             is_accepted = np.array([
@@ -45,62 +48,132 @@ class Algorithm:
         return np.array(result, dtype=np.float) / np.float(self.sample_rate)
 
 
-def process_new_input(filename):
-    algorithm = Algorithm(*wavfile.read(filename))
-    return algorithm.edges()
-
-
-executor = ProcessPoolExecutor(1)
+process_pool_executor = ProcessPoolExecutor(1)
 loop = asyncio.get_event_loop()
 song_edges = None
 
 
-async def spawn_job(content):
-    global song_edges
-    print('kok', flush=True)
+class TaskResultStorage:
+    def __init__(self):
+        self.result = None
+
+    @property
+    def is_ready(self):
+        return self.result is not None
+
+
+task_result_storage = TaskResultStorage()
+
+
+async def spawn_task(content):
     file = tempfile.NamedTemporaryFile()
     file.write(content)
-    result = await loop.run_in_executor(executor, process_new_input, file.name)
+    task = FileProcessingTask(file.name)
+    payload = partial(task.do_payload, task.generate_unique_task_id(), task.filename)
+    task_result_storage.result = await loop.run_in_executor(process_pool_executor, payload)
     file.close()
-    song_edges = result
 
 
-async def upload_handler(request):
-    print('Received file', flush=True)
+class FileProcessingTask:
+    def __init__(self, filename):
+        global task_result_storage
+        task_result_storage = TaskResultStorage()
+        self.filename = filename
+
+    @staticmethod
+    def generate_unique_task_id():
+        return 'default'
+
+    @staticmethod
+    def do_payload(unique_task_id, filename):
+        wav_filename = f'{unique_task_id}.wav'
+        # convert file to .wav
+        ffmpeg.input(filename).output(wav_filename).overwrite_output().run()
+        # load .wav
+        sample_rate, data = wavfile.read(wav_filename)
+        # precalculation
+        algo = PrecalcAlgo(sample_rate, data)
+        # return results
+        return algo.run()
+
+
+class NextJumpAlgo:
+    def __init__(self, result):
+        self.result = result
+
+    def roulette_selector(self, values):
+        values = np.array(values, dtype=np.float)
+        values /= np.sum(values)
+        cummulative = [0]
+        for dist_perc in values:
+            cummulative.append(dist_perc + cummulative[-1])
+        value = random.random()
+
+        def bin_find(value):
+            left = 0
+            right = len(cummulative) - 1
+            while right - left > 1:
+                middle = left + (right - left) // 2
+                if cummulative[middle] > value:
+                    right = middle
+                else:
+                    left = middle
+            return left
+
+        return bin_find(value)
+
+    def get_next_jump(self, current_time):
+        viable_options = self.result[[True if first > current_time or second > current_time else False for first, second in self.result]]
+
+        def distance(option):
+            return abs(option[0] - current_time) + abs(option[1] - current_time)
+
+        distances = np.array([distance(option) for option in viable_options], dtype=np.float)
+        return viable_options[self.roulette_selector(distances)]
+
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def index(_):
+    return web.Response(text='FEED US!')
+
+
+@routes.post('/upload')
+async def upload(request):
+    print('Started file upload...', flush=True)
+    start_time = time.time()
     content = await request.content.read()
-    print('Loaded file', flush=True)
-    asyncio.ensure_future(spawn_job(content), loop=loop)
+    end_time = time.time()
+    print(f'Finished file upload, done in {int(end_time - start_time)}ms.', flush=True)
+
+    asyncio.ensure_future(spawn_task(content), loop=loop)
     return web.Response(status=200)
 
 
-async def edges_handler(request):
-    if song_edges is None:
+@routes.post('/next')
+async def get_next(request):
+    current_time = (await request.json())['current_time']
+    if task_result_storage.is_ready:
+        algo = NextJumpAlgo(task_result_storage.result)
+        next_jump = algo.get_next_jump(current_time)
+        return web.json_response(data={'from': next_jump[0], 'to': next_jump[1]})
+    else:
         return web.Response(status=422)
-    return web.json_response(data=song_edges.tolist())
 
 
-async def main_page_handler(request):
-    return web.Response(status=200, content_type='application/text')
-
-
-async def audio(_):
-    return web.FileResponse('./tron.wav')
+app = web.Application()
+app.add_routes(routes)
+logging.basicConfig(level=logging.DEBUG)
 
 
 async def on_prepare(_, response):
     response.headers[hdrs.ACCESS_CONTROL_ALLOW_ORIGIN] = '*'
     response.headers[hdrs.ACCESS_CONTROL_ALLOW_METHODS] = 'OPTIONS,GET,POST'
 
-
-app = web.Application()
-logging.basicConfig(level=logging.DEBUG)
 app.on_response_prepare.append(on_prepare)
-app.add_routes([
-    web.get('/audio', audio),
-    web.post('/upload', upload_handler),
-    web.get('/edges', edges_handler),
-    web.get('/', main_page_handler)
-])
+
 
 if __name__ == '__main__':
     web.run_app(app, port=os.getenv('PORT', 5000))
