a89083c213744d7ed6fb0852e33e906ec99ff1d9
TurtlePU <sokolov.p64@gmail.com>
2019-10-27 01:03:35 +0300

    no info before completion

--- client/src/scripts/main.ts
+++ client/src/scripts/main.ts
@@ -73,6 +73,7 @@ async function onTrackInput() {
             if (json.ready) {
                 button.style.display = 'none';
                 timeline.style.display = 'block';
+                $('#info').style.display = 'block';
                 setInterval(() => {
                     $('#next-jump').innerText = `Next jump: ${round(lastJump.from)} >>> ${round(lastJump.to)}`;
                     $('#time-now').innerText = `Current time: ${round(audio.getTotalTime())}`;
