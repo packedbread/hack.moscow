5ab9792f7ab8c88f257495cae0f5c06ee81ea040
TurtlePU <sokolov.p64@gmail.com>
2019-10-27 01:04:31 +0300

    minor fix

--- client/src/scripts/main.ts
+++ client/src/scripts/main.ts
@@ -73,7 +73,7 @@ async function onTrackInput() {
             if (json.ready) {
                 button.style.display = 'none';
                 timeline.style.display = 'block';
-                $('#info').style.display = 'block';
+                $('#info').style.display = 'flex';
                 setInterval(() => {
                     $('#next-jump').innerText = `Next jump: ${round(lastJump.from)} >>> ${round(lastJump.to)}`;
                     $('#time-now').innerText = `Current time: ${round(audio.getTotalTime())}`;
