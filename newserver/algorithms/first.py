8d0dd1e89c27b6768daca844bc5f7fc13ad22319
lemondrok <dun2000@mail.ru>
2019-10-27 05:21:30 +0300

    erase tab

--- newserver/algorithms/first.py
+++ newserver/algorithms/first.py
@@ -2,7 +2,7 @@ from concurrent.futures import ThreadPoolExecutor
 from .abstract import AbstractJumpDetector
 from collections import defaultdict
 from scipy.fftpack import rfft
-from skimage import util
+from tqdm import tqdm
 import numpy as np
 import logging
 import time
@@ -26,58 +26,74 @@ class FirstJumpDetector(AbstractJumpDetector):
     def calc_hash(arr):
         ans = 0
         for i in range(len(arr)):
-            ans += arr[i]
+            ans += arr[i] * (len(arr) - i) / len(arr)
 
         return ans
 
     def _get_fft_hash(self, from_sample, window_size):
         freq_arr = rfft(self.one_channel[from_sample: from_sample + window_size])
-        windowed = util.view_as_blocks(freq_arr, (40,))
-        block_sum = np.array(list(map(np.sum, windowed)))
-
-        arr_hash = self.calc_hash(block_sum)
-        arr_hash = round(arr_hash, 4)
-
-        return [arr_hash, from_sample + window_size / 2]
+        x = np.argmax(freq_arr)
+        return x
 
     def run(self):
-        start_time = time.time()
-        window_size = 44000
-        stride = 1000
-        hash_result = []
+        start_time_os = time.time()
+        window_size = 22000
+        stride = 500
         result = []
+
+        start_time = len(self.one_channel) // 100
+        end_time = len(self.one_channel) - window_size - len(self.one_channel) // 100
+
+        hash_result = [0] * ((int(end_time) - int(start_time) + stride - 1) // int(stride))
+
         with ThreadPoolExecutor(16) as pool:
             def iteration(first_start):
                 hsh = self._get_fft_hash(first_start, window_size)
                 return hsh, first_start
 
-            # def map_append(index_list):
-            #     res = []
-            #     for i in range(len(index_list)):
-            #         for j in range(i + 1, len(index_list)):
-            #             if abs(index_list[j] - index_list[i]) > np.float(self.sample_rate) * 2:
-            #                 res.append([index_list[i], index_list[j]])
-            #     return res
+            for result_part in pool.map(iteration, range(start_time, end_time, stride)):
+                hash_result[(result_part[1] - start_time) // stride] = result_part[0]
 
-            for result_part in pool.map(
-                    iteration, range(
-                        len(self.one_channel) // 10,
-                        len(self.one_channel) - window_size - len(self.one_channel) // 10, stride
-                    )
-            ):
-                hash_result.append(result_part[0])
 
-        hash_result.sort()
-        max_hash = hash_result[-1][0]
-        for i in range(len(hash_result) - 1):
-            if hash_result[i + 1][0] - hash_result[i][0] < max_hash / 1000000:
-                result.append([hash_result[i][1], hash_result[i + 1][1]])
+        print(hash_result)
 
-        end_time = time.time()
-        print(f'Finished PrecalcMediumFFTAlgo precalc, done in {int((end_time - start_time) * 1000)}ms.', flush=True)
-        logger.critical(hash_result)
-        logger.critical(len(result))
-        print(hash_result, flush=True)
+        hash_map = defaultdict(lambda: [])
+        chunk_length = 30
+        temp, mod = int(0), int(117)
+        MOD = int(1e9 + 7)
+        kek = int(mod**(chunk_length - 1)) % MOD
+
+        # for i in range(len(hash_result)):
+        #     if hash_result[i] == 0:
+        #         print(i)
+
+        for i in range(chunk_length):
+            temp *= mod
+            temp %= MOD
+            temp = int(temp + hash_result[i])
+            temp %= MOD
+
+        hash_map[temp] = [0]
+
+        for i in range(1, len(hash_result) - chunk_length + 1):
+            temp -= kek * int(hash_result[i - 1])
+            temp = (temp + MOD) % MOD
+            temp *= mod
+            temp %= MOD
+            temp += hash_result[i + chunk_length - 1]
+            temp %= MOD
+
+            hash_map[temp].append(start_time + i * stride)
+
+        print(hash_map)
+
+        for _, item in tqdm(hash_map.items()):
+            for i in range(len(item)):
+                for j in range(i + 1, len(item)):
+                    result.append([item[i], item[j]])
+
+        end_time_os = time.time()
+        print(f'Finished PrecalcMediumFFTAlgo precalc, done in {int((end_time_os - start_time_os) * 1000)}ms.', flush=True)
         print(len(result), flush=True)
 
         return np.array(result, dtype=np.float) / np.float(self.sample_rate)
